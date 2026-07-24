fc3136191249750b5e2b63065ad3c1dd060d0bb6
Aaron <imlxaaron@gmail.com>
2026-07-24 14:56:39 +0800

    Update _worker.js

--- CORSAPI/_worker.js
+++ CORSAPI/_worker.js
@@ -24,9 +24,9 @@ const EXCLUDE_HEADERS = new Set([
 ])
 
 const JSON_SOURCES = {
-  'jin18': 'https://raw.githubusercontent.com/hafrey1/LunaTV-config/refs/heads/main/jin18.json',
-  'jingjian': 'https://raw.githubusercontent.com/hafrey1/LunaTV-config/refs/heads/main/jingjian.json',
-  'full': 'https://raw.githubusercontent.com/hafrey1/LunaTV-config/refs/heads/main/LunaTV-config.json'
+  'jin18': 'https://github.com/imlxaaron/LunaTV-config/blob/main/jin18.json',
+  'jingjian': 'https://github.com/imlxaaron/LunaTV-config/blob/main/jingjian.json',
+  'full': 'https://github.com/imlxaaron/LunaTV-config/blob/main/LunaTV-config.json'
 }
 
 const FORMAT_CONFIG = {
